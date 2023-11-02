7b792cbd91a68313ff8ed4131aac03359b0718ec
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-02 03:37:53 -0500

    fix: corrección aserciones

--- tests/commons/test_settings.py
+++ tests/commons/test_settings.py
@@ -5,10 +5,10 @@ import pytest
 class TestSettings:
     @pytest.mark.asyncio
     async def test_settings(self):
-        os.environ["customers_ms"] = "customers_url"
         os.environ["candidates_ms"] = "candidates_url"
+        os.environ["customers_ms"] = "customers_url"
 
         from app.commons.settings import settings
 
-        assert settings.candidates_ms == "customers_url"
-        assert settings.customers_ms == "candidates_url"
+        assert settings.candidates_ms == "candidates_url"
+        assert settings.customers_ms == "customers_url"
