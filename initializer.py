4a286f30a353c308e85a85b373252a2e7573081f
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-12 19:10:01 -0500

    fix: exposicion nuevo endpoint de entrevistas

--- initializer.py
+++ initializer.py
@@ -4,6 +4,8 @@ from app.client.repositories.candidate_repository import CandidateRepository
 from app.client.repositories.customer_repository import CustomerRepository
 from app.health.controllers import health_controller
 from app.health.services.health_service import HealthService
+from app.interview.controllers import interview_controller
+from app.interview.services.interview_service import InterviewService
 from app.position.controllers import position_controller
 from app.position.services.position_service import PositionService
 
@@ -14,6 +16,7 @@ class Initializer:
 
     def setup(self):
         self.init_health_module()
+        self.init_interview_module()
         self.init_position_module()
 
     def init_health_module(self):
@@ -28,3 +31,11 @@ class Initializer:
         position_service = PositionService(candidate_repository, customer_repository)
         position_controller.initialize(position_service)
         self.app.include_router(position_controller.router)
+
+    def init_interview_module(self):
+        print("Initializing interview module")
+        candidate_repository = CandidateRepository()
+        customer_repository = CustomerRepository()
+        interview_service = InterviewService(candidate_repository, customer_repository)
+        interview_controller.initialize(interview_service)
+        self.app.include_router(interview_controller.router)
